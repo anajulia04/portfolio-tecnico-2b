2b93a5c2ed03254b6f261f64d800ff20d168c421
anajulia04 <83764612+anajulia04@users.noreply.github.com>
2022-04-28 10:53:07 -0300

    adicionado atividade1
    atividade 1 trimestre, visualizações sql;

---
+++ Banco_de_Dados/atividade1.sql
@@ -0,0 +1,11 @@
+select.release_date, distributor, original_game_publisher from film_adaptations where release_date > '2002-02-02' 
+—-------------------------—------------------------- 
+select * from film_adaptations where metacritic > 32 
+—-------------------------—------------------------- 
+select * film_adaptations where distributor = 'nintendo’ 
+—-------------------------—------------------------- 
+/Games produzidos pela Midway ou a linha do Mortal Kombat/ 
+—-------------------------—------------------------- 
+select * from Film_adaptations where original_game_publisher = Eidos 
+—-------------------------—------------------------- 
+select * from film_adaptations
